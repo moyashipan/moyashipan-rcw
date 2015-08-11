a5f4bd4b4de77f71b419dc7c727f6e90c4ef3d29
moyashipan <moyashipan@gmail.com>
2015-08-11 12:23:23 +0900

    spec3: レシピ情報を別ファイル(yaml)で管理

--- recipe.rb
+++ recipe.rb
@@ -1,2 +1,17 @@
-TITLE = 'オムライス'.freeze
-puts TITLE
+require 'yaml'
+
+def parse_recipe_path!
+  unless ARGV.length == 1
+    puts <<-'EOS'
+Invalid args.
+Run like this: `ruby recipe.rb recipe.yaml`
+    EOS
+    exit
+  end
+
+  ARGV[0]
+end
+
+recipe_path = parse_recipe_path!
+recipe_titles = YAML.load_file(File.join(__dir__, recipe_path))
+puts recipe_titles.join("\n")
